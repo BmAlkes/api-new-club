cfee2cb10669d44b38f197c5489b097a229227ca
bmalkes <bmalkes@gmail.com>
2023-01-23 13:02:47 +0200

    push new test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://api-new-club.vercel.app"],
+    origin: ["http://localhost:5173", "https://new-club-clothes.vercel.app"],
     credentials: true,
   })
 );
